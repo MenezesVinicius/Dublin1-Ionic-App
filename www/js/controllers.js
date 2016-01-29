1b15a82154b764c3d77e60386475951f65dd3470
Vinicius Correa Menezes <vicmen33@hotmail.com>
2016-01-29 22:16:23 +0000

    Consegui pegar os dados da API e transformar para JSON

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,39 @@
-angular.module('starter.controllers', [])
+var app = angular.module('starter.controllers', []);
 
-.controller('DashCtrl', function($scope) {})
+app.controller('HomeCtrl', function ($scope, FacilitiesService) {
+    FacilitiesService.findAll().then(function (facilities) {
+        console.log(facilities.data);
+        var x2js = new X2JS();
+        var jsonObject = x2js.xml_str2json(facilities.data);
+        console.log(jsonObject.markers);
+        angular.forEach(jsonObject.markers.marker, function (value, key) {
+            console.log("Name: " + value.name);
+        })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+    })
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+app.controller('ChatsCtrl', function ($scope, Chats) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+        Chats.remove(chat);
+    };
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+app.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+})
+
+app.controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+        enableFriends: true
+    };
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,90 @@
-angular.module('starter.services', [])
+var keyAPI = "ef1b2b0d52a115f0a9d8b8a6e92b4119";
+var baseURL = "http://www.dublin.ie/maps/api.php?apikey=" + keyAPI;
+var app = angular.module('starter.services', []);
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+app.factory('Chats', function () {
+    // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+    // Some fake testing data
+    var chats = [{
+        id: 0,
+        name: 'Ben Sparrow',
+        lastText: 'You on your way?',
+        face: 'img/ben.png'
+    }, {
+        id: 1,
+        name: 'Max Lynx',
+        lastText: 'Hey, it\'s me',
+        face: 'img/max.png'
+    }, {
+        id: 2,
+        name: 'Adam Bradleyson',
+        lastText: 'I should buy a boat',
+        face: 'img/adam.jpg'
+    }, {
+        id: 3,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'img/perry.png'
+    }, {
+        id: 4,
+        name: 'Mike Harrington',
+        lastText: 'This is wicked good ice cream.',
+        face: 'img/mike.png'
+    }];
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    return {
+        all: function () {
+            return chats;
+        },
+        remove: function (chat) {
+            chats.splice(chats.indexOf(chat), 1);
+        },
+        get: function (chatId) {
+            for (var i = 0; i < chats.length; i++) {
+                if (chats[i].id === parseInt(chatId)) {
+                    return chats[i];
+                }
+            }
+            return null;
         }
-      }
-      return null;
+    };
+});
+
+app.factory('FacilitiesService', function ($http) {
+    var Categories = [];
+
+    return {
+        findAll: function () {
+            var promise = $http({
+                method: 'GET',
+                url: baseURL + "&action=getFacilities",
+                crossDomain: true
+            });
+
+            promise.success(function (data) {
+
+            }).error(function (err) {
+                console.log(err);
+            });
+            return promise;
+        },
+
+        findById: function (vendedorId) {
+            for (var i = 0; i < Vendedores.length; i++) {
+                if (Vendedores[i].id == vendedorId) {
+                    var vendedor = Vendedores[i];
+                    if (vendedor.ddd == 48) {
+                        vendedor.ddd = "";
+                    }
+                    else if (vendedor.ddd != "" && vendedor.ddd.length == 2) {
+                        vendedor.ddd = "041" + vendedor.ddd;
+                    }
+
+                    console.log(vendedor.nome);
+                    return vendedor;
+                }
+            }
+        }
     }
-  };
 });
